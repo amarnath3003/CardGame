eb2021aba3b904ac9899ac3cb7ec1338ae073546
Amarnath <amarnathdevraj2005@gmail.com>
2026-05-11 19:32:33 +0530

    python logic

--- CardsGame.py
+++ CardsGame.py
@@ -1,102 +1,35 @@
 import random
-deck =  ["As","2s","3s","4s","5s","6s","7s","8s","9s","10s","Ks","Qs","Js","Ac","2c","3c","4c","5c","6c","7c","8c","9c","10c","Kc","Qc","Jc"
-         ,"Ah","2h","3h","4h","5h","6h","7h","8h","9h","10h","Kh","Qh","Jh","Ad","2d","3d","4d","5d","6d","7d","8d","9d","10d","Kd","Qd","Jd"]
-
-card_rank_priority = {
-    'A': 14, 'K': 13, 'Q': 12, 'J': 11, '10': 10,
-    '9': 9, '8': 8, '7': 7, '6': 6, '5': 5,
-    '4': 4, '3': 3, '2': 2
-}
-
-def get_card_priority(card):
-    # Extract rank from card ('As' -> 'A', '10c' -> '10')
-    rank = card[:-1]
-    return card_rank_priority[rank]
-
-# Create a new deck with priorities
-priority_deck = {card: get_card_priority(card) for card in deck}
-
-p1 = []
-p2 = []
-p3 = []
-p4 = []
-
-
-for i in range(0, len(deck), 4):
-
-    x = random.choice(deck)
-    p1.append(x)
-    deck.remove(x)
-
-    x = random.choice(deck)
-    p2.append(x)
-    deck.remove(x)
-
-    x = random.choice(deck)
-    p3.append(x)
-    deck.remove(x)
-
-    x = random.choice(deck)
-    p4.append(x)
-    deck.remove(x)
-
-mideck = []
-element = "As"
-lists = [p1,p2,p3,p4]
-
-for j in [p1,p2,p3,p4]:
-    if element in j:
-        mideck.append(element)
-        j.remove(element)
-        lists = lists[i:] + lists[:i]
-        break
-
-print(mideck)
-
-p1, p2, p3, p4 = lists
-'''print("p1:", p1)
-print("p2:", p2)
-print("p3:", p3)
-print("p4:", p4)'''
-
-current_turn = 1
-turn_counter = 1
-garbage_deck = []
-highest_priority = 0
-
-
-while True: 
-   player_turn = lists[current_turn]
-   print(player_turn)
-   pl_input = input("Enter the card you want to Place")
-   if pl_input in player_turn:
-       mideck.append(pl_input)
-       player_turn.remove(pl_input)
-       current_turn = (current_turn + 1) % len(lists)
-       turn_counter = turn_counter + 1
-       print(turn_counter)
-       if turn_counter == 4:
-           garbage_deck.extend(mideck)
-           for card in mideck:
-               priority = priority_deck[card]
-               if priority > highest_priority:
-                highest_priority = priority
-                highest_priority_card = card
-           print(highest_priority_card)
-           mideck.clear()
-           print("Round Over")
-           break
-
-
-   
-
-   print("Middle deck:", mideck)
-   print("Remaining Cards",player_turn)
-   print("Next player's Cards:", lists[current_turn])
-   
-print("Middle deck:", mideck)
 
 
 
+# Full Deck: [Weight, Suit_ID]
+deck = [
+    # Clubs (ID: 0)
+    [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0], [13, 0], [14, 0],
+    
+    # Diamonds (ID: 1)
+    [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1], [14, 1],
+    
+    # Hearts (ID: 2)
+    [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [13, 2], [14, 2],
+    
+    # Spades (ID: 3)
+    [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3], [12, 3], [13, 3], [14, 3]
+]
 
-   
+Player1 = []
+Player2 = []
+Player3 = []
+Player4 = []
+
+def shuffle_deck(deck):
+    for i in range(len(deck)//4):
+        Player1.append(deck.pop(random.randint(0, len(deck)-1)))
+        Player2.append(deck.pop(random.randint(0, len(deck)-1)))
+        Player3.append(deck.pop(random.randint(0, len(deck)-1)))
+        Player4.append(deck.pop(random.randint(0, len(deck)-1)))
+    print(Player1,"\n",Player2,"\n",Player3,"\n",Player4)
+        
+    return deck
+
+shuffle_deck(deck)
